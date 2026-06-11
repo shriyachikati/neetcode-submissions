3986a996457fd691e3efd60d8b767a57af76f783
Shriya Chikati <95562069+shriyachikati@users.noreply.github.com>
2026-06-11 01:05:45 -0500

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,24 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Use binary search on the piles array for the eating speed
+        piles.sort()
+        l = 1
+        r = max(piles)
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            time = 0
+            for pile in piles:
+                time += math.ceil(pile / mid)
+
+            if time > h:
+                l = mid + 1
+
+            else:
+                output = mid
+                r = mid - 1
+
+        return output
